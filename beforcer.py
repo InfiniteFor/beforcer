86b088fe2ac38630cfcb4f402a47bc4a01fb0879
InfiniteFor <wesleyvdkamp@gmail.com>
2018-07-03 15:31:38 +0200

    Add files via upload

---
+++ beforcer.py
@@ -0,0 +1,31 @@
+import bcrypt
+from threading import Thread
+
+def crackhash(password,hashed,user):
+    if bcrypt.hashpw(password, hashed) == hashed:
+        with open('pass.txt', 'a') as pf:
+            pf.write(user + ':' + password + '\n')
+            print '[+] Password for %s is %s' %(user,password)
+        
+def main():
+    hash_list = 'bcrypt_hashes.txt'
+    dictionary = 'top10.txt'
+    print '[*] Start brute-force with threading...'
+    with open(hash_list,'r') as hl, open(dictionary,'r') as dic:
+        hl = hl.readlines()
+        dic = dic.readlines()
+        for idx,hashes in enumerate(hl):
+            split = hashes.split(':')
+            hashed = split[1].split('\n')[0]
+            user = split[0]
+            if idx % 1000 == 0 and idx != 0:
+                print 'Compared %s users with complete dictionary' %(idx)
+            for passwords in dic:
+                password = passwords.strip('\n')
+                t = Thread(target=crackhash, args=(password, hashed, user))
+                t.start()
+    print '\nFinished!'
+        
+
+if __name__ == '__main__':
+    main()
